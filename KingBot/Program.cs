7d33fa16bf324f60ec53c523cd93a77d77416f0c
Cody Shafer <cody.shafer@gmail.com>
2017-04-08 16:12:17 -0400

    Basic connection setup

--- KingBot/Program.cs
+++ KingBot/Program.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+
 
 namespace KingBot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World");
+
+            var client = new DiscordClient();
+            await client.Connect(PrivateKey.Token, TokenType.Bot);
             Console.ReadLine();
+            await client.Disconnect();
+
+
+
+
         }
+
     }
 }
